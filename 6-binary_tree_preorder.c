78f9d51392caaca2bd8f665d365f422c65a5b7fe
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-01 20:42:14 +0000

    Adding task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - prints the tree in pre order
+ * @tree: tree to print
+ * @func: pointer to function
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (binary_tree_is_leaf(tree) == 0)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+		return;
+	}
+	else
+	{
+		func(tree->n);
+		return;
+	}
+
+}
