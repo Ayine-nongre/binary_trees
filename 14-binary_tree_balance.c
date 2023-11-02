690639cf9bf4e3964ff5183404618b8df30ffd09
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-02 11:40:51 +0000

    Editing task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * tree_height - gets the height of a tree
+ * @tree: tree argument
+ * Return: size_t value
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left_side, right_side;
+
+	if (tree == NULL)
+		return (0);
+
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+	if (left_side >= right_side)
+		return (left_side + 1);
+	else
+		return (right_side + 1);
+}
+
+/**
  * binary_tree_balance - calculate the balance of a tree
  * @tree: tree argument
  * Return: int value
@@ -13,8 +34,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_side = binary_tree_height(tree->left);
-	right_side = binary_tree_height(tree->right);
+	left_side = tree_height(tree->left);
+	right_side = tree_height(tree->right);
 
 	balance = left_side - right_side;
 	return (balance);
