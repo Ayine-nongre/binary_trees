66553c8de636aa2bf7f9ee3c792be0843be94348
Ayine-nongre <eugeneatinbire@gmail.com>
2023-11-02 10:42:50 +0000

    Editing task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,16 +11,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	if (binary_tree_is_leaf(tree) == 0)
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
-		return;
-	}
-	else
-	{
-		func(tree->n);
-		return;
-	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+
+	return;
 }
