526248ffbcd40aa5792cc571434166fe4045ed5c
Ernest-Geek <ernestampene1@gmail.com>
2023-11-03 07:28:28 -0700

    Fix segmentation fault issue by addressing memory access violations.

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "binary_trees.h"
 /**
  * code Description - A function that inserts a node as the left-child of another node
  * @value: The value stored in the node
  * @left: Left node
  * @Right: Right node
 **/
-/**Define the structure for the binary node**/
-typedef struct binary_tree
-{
-/**stores the value of the node**/
-        int value;
-/**pointer pointing to the left child**/
-        struct binary_tree *left;
-/**pointer to the right child**/
-        struct binary_tree *right;
-} binary_tree_t;
+
 /**A function that deletes the entire binary tree node**/
 void binary_tree_delete(binary_tree_t *tree)
 {
 /**Check if the tree is null**/
-        if (!tree)
+        if (tree->left != NULL && tree->right != NULL)
         {
+        /**Deleting left and right trees**/
+            binary_tree_delete(tree->left);
+            binary_tree_delete(tree->right);
+            free(tree);
             return;
         }
-/**Deleting left and right trees**/
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
+        if (tree->left != NULL && tree->right == NULL)
+        {
+            binary_tree_delete(tree->left);
+            free(tree);
+            return;
+        }
+        if (tree->right != NULL && tree->left == NULL)
+        {
+            binary_tree_delete(tree->right);
+            free(tree);
+            return;
+        }
+        if (tree->right == NULL && tree->left == NULL)
+        {
+            free(tree);
+            return;
+        }
 }
-
