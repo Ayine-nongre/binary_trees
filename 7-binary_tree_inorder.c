602f224d001a43d568fbfe8af5a15aeb088f871c
Ernest-Geek <ernestampene1@gmail.com>
2023-11-03 15:48:38 -0700

    Refactor binary_tree_inorder function and improve documentation.

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,23 +2,10 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
- * code description: a function that goes through a binary 
- * tree using in-order traversal
- * @right: Representing the right node
- * @left: Representing the left node
- * @item: Number of items in the node
- **/
-/**Defining a structure for the node binary tree**/
-struct node
-{
-	int n;
-	struct node *left;
-	struct node *right;
-};
-/**
- * @tree: A pointer to the root node
+ * binary_tree_inorder - In-order traversal of a binary tree
+ * code description - A function that goes through a binary tree
+ * @tree: A pointer to the root nod of the tree
  * @func: A pointer to a function
- * Return: nothing
  **/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
